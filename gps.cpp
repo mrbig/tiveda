4a22a45932506caa92c10c02dcb0110149c4933c
Nagy Attila Gabor <mrbig@sneaker.hu>
2016-04-18 00:45:24 +0200

    GPS data parsing

--- gps.cpp
+++ gps.cpp
@@ -1,6 +1,16 @@
 #include "gps.h"
 
+// Pointer to the incoming serial message buffer
 String* GPS::message;
+
+// Current status: true if we have reception
+boolean GPS::reception;
+
+// We keep the last valid state from gps here
+GPSSTATUS GPS::last;
+
+// Reference to the eventManager
+EventManager* GPS::eventManager;
 
 /**
  * Parse incoming messages. This gets called once a complete
@@ -15,8 +25,108 @@ void GPS::handleMessage(int eventCode, int eventParam) {
     }
     if (message->startsWith("$GPRMC,")) {
         Serial.print("> ");
-        Serial.print(*message);
+        Serial.println(*message);
+        parseRMC(message);
     }
+}
+
+/**
+ * Handle GPRMC messages
+ */
+void GPS::parseRMC(String* msg) {
+    byte offset = 7;
+    byte fieldNr = 0;
+    String current;
+    boolean old;
+
+    float rawCoord, rawSpd, rawHdg;
+    char rawIndicator;
+
+    while (fieldNr < 8) {
+        current = msg->substring(offset);
+        switch (fieldNr) {
+            case 1:
+                old = reception;
+                if (current.charAt(0) == 'A') {
+                    reception = true;
+                    last.timestamp = millis();
+                } else if (current.charAt(0) == 'V') {
+                    reception = false;
+                }
+                if (old != reception) {
+                    eventManager->queueEvent(GPS_STATUS_CHANGED, reception);
+                }
+                break;
+
+            case 2:
+            case 4:
+                rawCoord = current.toFloat();
+                break;
+
+            case 3:
+            case 5:
+                if (!rawCoord) break;
+                rawIndicator = current.charAt(0);
+
+                if (fieldNr == 3) {
+                    last.lat = toCoordinates(rawCoord, rawIndicator);
+                } else {
+                    last.lng = toCoordinates(rawCoord, rawIndicator);
+                }
+
+                break;
+
+            case 6:
+                if (current.indexOf(',') == 0) {
+                    last.spd = -1;
+                    break;
+                }
+                last.spd = current.toFloat() * 1.852;
+                break;
+
+            case 7:
+                if (current.indexOf(',') == 0) {
+                    last.hdg = -1;
+                    break;
+                }
+                last.hdg = current.toFloat();
+        }
+        
+        offset = msg->indexOf(',', offset) + 1;
+        fieldNr++;
+    }
+
+    Serial.print("Lat: ");
+    Serial.println(last.lat,6);
+    Serial.print("Lng: ");
+    Serial.println(last.lng,6);
+    Serial.print("SPD: ");
+    Serial.println(last.spd);
+    Serial.print("HDG: ");
+    Serial.println(last.hdg);
+}
+
+/**
+ * Convert NMEA coordinates to defimal representation
+ * @param float the coordinate in ddmm.mmmm format
+ * @param char the N/s/E/W indicator
+ * @return float the converted value
+ */
+float GPS::toCoordinates(float coord, char indicator) {
+    byte deg;
+    float min, dec;
+
+    deg = coord / 100;
+    min = coord - (deg * 100);
+    
+    dec = deg + min/60;
+   
+    if (indicator == 'W' || indicator == 'S') {
+        dec = - dec;
+    }
+
+    return dec;
+   
 }
 
 /**
--- gps.h
+++ gps.h
@@ -5,6 +5,19 @@
 #include "config.h"
 #include <EventManager.h>
 
+struct GPSSTATUS {
+    // Latitude
+    float lat;
+    // Longitude
+    float lng;
+    // Heading
+    float hdg;
+    // Speed
+    float spd;
+    // Timestamp of last valid gps data
+    unsigned long timestamp;
+};
+
 class GPS {
     public:
 
@@ -12,7 +25,8 @@ class GPS {
          * Register event handlers
          * @param EventManager the event manager used for events
          */
-        static void init(EventManager* eventManager, String* buff) {
+        static void init(EventManager* em, String* buff) {
+            eventManager = em;
             eventManager->addListener(GPS_MESSAGE_RECEIVED, &GPS::handleMessage);
             message = buff;
         };
@@ -27,10 +41,33 @@ class GPS {
         // Reference to the message buffer
         static String* message;
 
+        // true, if we have reception
+        static boolean reception;
+
+        // Latest latitude
+        static GPSSTATUS last;
+
+        // Reference to the event manager
+        static EventManager* eventManager;
+        
+
         /**
          * Calculate the checksum of the NMEA sentence
          */
         static byte calculateChecksum();
+
+        /**
+         * Parse GPRMC messages
+         */
+        static void parseRMC(String* msg);
+
+        /**
+         * Convert NMEA coordinates to defimal representation
+         * @param float the coordinate in ddmm.mmmm format
+         * @param char the N/s/E/W indicator
+         * @return float the converted value
+         */
+        static float toCoordinates(float coord, char inverse);
 
 };
 
