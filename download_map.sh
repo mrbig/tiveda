dfaa2a1fd9ddb56e6e69efaa0714532a4d64caf5
Nagy Attila Gábor <mrbig@sneaker.hu>
2016-09-02 10:15:47 +0200

    updated downloadmap.sh

--- download_map.sh
+++ download_map.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mkspiffs  -c data -p 256 -b 8192 -s 32768 /tmp/proba.spiffs.bin
-esptool -cd nodemcu -cb 921600 -cp /dev/ttyUSB0 -ca 0x100000 -cf /tmp/proba.spiffs.bin
+mkspiffs  -c data -p 256 -b 8192 -s 32768 /tmp/map.spiffs.bin
+esptool -cd nodemcu -cb 921600 -cp /dev/ttyUSB0 -ca 0x100000 -cf /tmp/map.spiffs.bin
